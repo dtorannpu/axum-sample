c0dfd774c7dc39ab07b07a537e30266c22fb9360
dtorannpu <dtorannpu@gmail.com>
2025-12-14 13:30:33 +0900

    Change server port from 3000 to 8080 and use `SocketAddr` for binding.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::net::{Ipv4Addr, SocketAddr};
 use anyhow::{Context, Result};
 use axum::{Router, routing::get};
 
@@ -7,7 +8,8 @@ async fn main() -> Result<()> {
 
     let app = Router::new().route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.context("Failed to bind")?;
+    let addr = SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 8080);
+    let listener = tokio::net::TcpListener::bind(addr).await.context("Failed to bind")?;
     axum::serve(listener, app).await.context("Failed to start server")
 }
 
