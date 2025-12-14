575805b5b7e9af9e42587871fe895110fafc648b
dtorannpu <dtorannpu@gmail.com>
2025-12-14 15:25:09 +0900

    Add OpenTelemetry tracing and Jaeger support
    Set up OpenTelemetry for tracing, added dependencies to `Cargo.toml`, and configured a Jaeger instance in `compose.yaml`. Enhanced the HTTP server with tracing layers and a sample endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 use anyhow::{Context, Result};
 use axum::{Router, routing::get};
+use opentelemetry::KeyValue;
+use opentelemetry::trace::TracerProvider;
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::{RandomIdGenerator, Sampler, SdkTracerProvider};
+use opentelemetry_semantic_conventions::{
+    SCHEMA_URL,
+    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, SERVICE_VERSION},
+};
 use std::net::{Ipv4Addr, SocketAddr};
+use tower_http::LatencyUnit;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::Level;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    init_tracing_subscriber()?;
 
-    let app = Router::new()
-        .route("/", get(root));
+    let router = Router::new()
+        .route("/", get(root))
+        .route("/sample", get(sample));
+    let app = router
+        .layer(
+            TraceLayer::new_for_grpc()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Millis),
+                ),
+        );
 
     let addr = SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 8080);
     tracing::info!("Listening on {}", addr);
@@ -19,6 +45,55 @@ async fn main() -> Result<()> {
         .context("Failed to start server")
 }
 
+fn resource() -> Resource {
+    Resource::builder()
+        .with_service_name("axum-sample")
+        .with_schema_url(
+            [
+                KeyValue::new(SERVICE_VERSION, "1.0.0"),
+                KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
+            ],
+            SCHEMA_URL,
+        )
+        .build()
+}
+
+fn init_tracer_provider() -> Result<SdkTracerProvider> {
+    let exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .build()?;
+
+    Ok(SdkTracerProvider::builder()
+        // Customize sampling strategy
+        .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+            1.0,
+        ))))
+        // If export trace to AWS X-Ray, you can use XrayIdGenerator
+        .with_id_generator(RandomIdGenerator::default())
+        .with_resource(resource())
+        .with_batch_exporter(exporter)
+        .build())
+}
+
+fn init_tracing_subscriber() -> Result<()> {
+    let tracer_provider = init_tracer_provider()?;
+
+    let tracer = tracer_provider.tracer("axum-sample-tracer");
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::filter::LevelFilter::from_level(
+            Level::INFO,
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .with(OpenTelemetryLayer::new(tracer))
+        .init();
+
+    Ok(())
+}
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn sample() -> &'static str {
+    "Hello, Sample!"
 }
