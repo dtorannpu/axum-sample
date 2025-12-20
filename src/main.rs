d98cbe21b89fcefbea6823c1954134a5ec7b03b7
dtorannpu <dtorannpu@gmail.com>
2025-12-20 15:57:38 +0900

    Add graceful shutdown support and update `tokio` features in `Cargo.toml`
    Integrated `signal-hook-registry` for handling signals, updated `tokio` to include the `signal` feature, and implemented a graceful shutdown mechanism with OpenTelemetry span purging.

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    init_tracing_subscriber()?;
+    let tracer_provider = init_tracing_subscriber()?;
 
     let router = Router::new()
         .route("/", get(root))
@@ -53,6 +53,7 @@ async fn main() -> Result<()> {
         .await
         .context("Failed to bind")?;
     axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal(tracer_provider))
         .await
         .context("Failed to start server")
 }
@@ -87,7 +88,7 @@ fn init_tracer_provider() -> Result<SdkTracerProvider> {
         .build())
 }
 
-fn init_tracing_subscriber() -> Result<()> {
+fn init_tracing_subscriber() -> Result<SdkTracerProvider> {
     let tracer_provider = init_tracer_provider()?;
 
     let tracer = tracer_provider.tracer("axum-sample-tracer");
@@ -100,8 +101,45 @@ fn init_tracing_subscriber() -> Result<()> {
         .with(OpenTelemetryLayer::new(tracer))
         .init();
 
-    Ok(())
+    Ok(tracer_provider)
 }
+
+async fn shutdown_signal(tracer_provider: SdkTracerProvider) {
+    fn purge_spans(tracer_provider: &SdkTracerProvider) {
+        tracer_provider
+            .shutdown()
+            .expect("Failed to shut down tracer provider");
+    }
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install signal handler")
+            .recv()
+            .await
+            .expect("Failed to receive SIGTERM signal");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c=>{
+            tracing::info!("Ctrl+C を受信しました。");
+            purge_spans(&tracer_provider);
+        },
+        _ = terminate=>{
+            tracing::info!("SIGTERM を受信しました。");
+            purge_spans(&tracer_provider);
+        },
+    }
+}
+
 async fn root() -> &'static str {
     "Hello, World!"
 }
