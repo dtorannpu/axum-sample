78f083e8092036b3fd8e2fa845c8563319d8f432
dtorannpu <dtorannpu@gmail.com>
2025-12-27 14:41:21 +0900

    Remove `AppResult` and `AppError` from `shared` crate and simplify `register` handler in `sample.rs`

--- api/src/handler/sample.rs
+++ api/src/handler/sample.rs
@@ -1,13 +1,12 @@
 use crate::model::sample::SampleRequest;
-use axum::Json;
 use axum::http::StatusCode;
+use axum::Json;
 use axum_valid::Garde;
-use shared::error::AppResult;
 
 pub async fn sample() -> &'static str {
     "Hello, Sample!"
 }
 
-pub async fn register(Garde(Json(_)): Garde<Json<SampleRequest>>) -> AppResult<StatusCode> {
-    Ok(StatusCode::CREATED)
+pub async fn register(Garde(Json(_)): Garde<Json<SampleRequest>>) -> StatusCode {
+    StatusCode::CREATED
 }
